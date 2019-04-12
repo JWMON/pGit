23d04438f76555cf55de3d38f8b2eea5dbace634
Joowon <danier123@gmail.com>
2019-04-12 11:05:20 +0900

    sub2

--- main.c
+++ main.c
@@ -5,6 +5,15 @@ void sub2();
 void sub3();
 
 int main(){
+	void sub2(){
+		for(int i=0; i<10; i++){
+			for(int j=0; j<10; j++){
+				printf("*");
+			}
+		printf("\n");
+		}
+	};
+	
 	sub1();
 	sub2();
 	sub3();
